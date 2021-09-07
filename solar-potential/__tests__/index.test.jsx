b14546eda1b725937a4e4ffa75d84311949a6f5f
John Anwana <jayanwana@gmail.com>
2021-09-07 13:39:05 +0100

    write login page tests

--- solar-potential/__tests__/index.test.jsx
+++ solar-potential/__tests__/index.test.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {describe, expect, test} from '@jest/globals'
 import { render, screen, fireEvent } from '@testing-library/react'
 import LoginPage from '../pages/index'
 
 describe('LoginPage Tests', () => {
-  it('renders a heading', () => {
+  test('renders a heading', () => {
     render(<LoginPage />)
 
     const heading = screen.getByRole('heading', {
@@ -12,7 +13,7 @@ describe('LoginPage Tests', () => {
     expect(heading).toBeInTheDocument()
   })
 
-  it('Renders an email input', () => {
+  test('Renders an email input', () => {
     render(<LoginPage />)
 
     const email = screen.getByRole('textbox', { name: '' })
@@ -20,25 +21,39 @@ describe('LoginPage Tests', () => {
     expect(email).toBeInTheDocument()
   })
 
-  it('email accepts input', () => {
+  test('email accepts input', () => {
     render(<LoginPage />)
 
     const email = screen.getByRole('textbox', { name: '' })
     fireEvent.change(email, { target: { value: 'test@email.com' } })
     expect(email.value).toBe('test@email.com')
   })
-  it('renders a submit button', () => {
+  test('renders a submit button', () => {
     render(<LoginPage />)
 
     const button = screen.getByRole('button', { name: /Sign in/i })
 
     expect(email).toBeInTheDocument()
   })
-  it('renders a link to the forgot password page', () => {
+  test('renders a link to the forgot password page', () => {
     render(<LoginPage />)
 
     const link = screen.getByRole('link', { name: /Forgot password/i })
 
     expect(link).toBeInTheDocument()
+  })
+  test('renders a link with correct href', () => {
+    render(<LoginPage />)
+
+    const link = screen.getByRole('link', { name: /Forgot password/i }).closest('a')
+
+    expect(link).toHaveAttribute('href', '/forget-password')
+  })
+  test('renders a link to the sign up with correct href', () => {
+    render(<LoginPage />)
+
+    const signUpLink = screen.getByRole('link', { name: /Dont have an account\? Sign up!/i }).closest('a')
+
+    expect(signUpLink).toHaveAttribute('href', '/signup')
   })
 })
